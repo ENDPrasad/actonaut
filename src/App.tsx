e511e57e1dca2f345931baf50b11e9280eaf59cc
endprasad <e.n.d.prasad@gmail.com>
2025-07-31 00:01:54 +0530

    Remove challenges route

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import CategoriesPage from './pages/CategoriesPage/CategoriesPage';
 import { ConceptPage } from './components/categories/ConceptPage';
 import HomePage from './concepts/ExamplePage';
 import ConceptOutlinePage from './pages/ConceptPage/ConceptPage';
-import { ChallengesPage } from './pages/challenges/ChallengesPage';
-import { ChallengePage } from './pages/challenges/ChallengePage';
+// import { ChallengesPage } from './pages/challenges/ChallengesPage';
+// import { ChallengePage } from './pages/challenges/ChallengePage';
 import ScrollToTop from './components/smallComponents/ScrollToTop';
 
 function App() {
@@ -20,8 +20,8 @@ function App() {
         <Route path='/explore' element={<CategoriesPage />}/>
         <Route path='/example' element={<HomePage />}/>
         <Route path='/explore/:slug' element={<ConceptOutlinePage><ConceptPage /></ConceptOutlinePage>}/>
-        <Route path="/challenges" element={<ChallengesPage />} />
-            <Route path="/challenge/:challengeId" element={<ChallengePage />} />
+        {/* <Route path="/challenges" element={<ChallengesPage />} />
+            <Route path="/challenge/:challengeId" element={<ChallengePage />} /> */}
       </Routes>
     </>
   )
